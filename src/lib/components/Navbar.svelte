0d151aeef183623ae1e50d6ee2e29438551fa5b4
aarsh21 <aarshpersonal21@gmail.com>
2025-02-21 17:03:27 +0530

    feat: add vesper colorscheme

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/stores';
 	const styles =
-		'text-cat_primary underline decoration-cat_quaternary decoration-wavy decoration-[1.7px] underline-offset-[7px] ';
+		'text-white_primary underline decoration-cat_primary decoration-wavy decoration-[1.7px] underline-offset-[7px] ';
 </script>
 
-<aside class="tracking-light mb-8 mt-8">
+<aside class="tracking-light mt-8 mb-8">
 	<nav class="flex scroll-pr-6 flex-row items-start gap-x-4 px-0 py-0 text-xl font-bold">
 		<a href="/" class={$page.route.id === '/(app)' ? `${styles}` : 'hover:text-cat_quaternary'}
 			>home</a
