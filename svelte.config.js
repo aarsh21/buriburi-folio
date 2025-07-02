3cb3430f9571c98b9d90201a898907f6ec557173
Aarsh Padia <hyorinmaru@Aarshs-MacBook-Air.local>
2025-07-02 22:06:12 +0530

    chore: add node adapter

--- svelte.config.js
+++ svelte.config.js
@@ -3,54 +3,54 @@ import remarkUnwrapImages from 'remark-unwrap-images';
 import remarkToc from 'remark-toc';
 import rehypeSlug from 'rehype-slug';
 
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import { bundledLanguages, createHighlighter } from 'shiki/bundle/web';
 
 const highlighterPromise = createHighlighter({
-	themes: ['vesper'],
-	langs: Object.keys(bundledLanguages)
+  themes: ['vesper'],
+  langs: Object.keys(bundledLanguages)
 });
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-	extensions: ['.md'],
-	layout: {
-		_: './src/mdsvex.svelte'
-	},
-	highlight: {
-		highlighter: async (code, lang = 'text') => {
-			// Reuse the same highlighter instance
-			const highlighter = await highlighterPromise;
-			const html = escapeSvelte(
-				highlighter.codeToHtml(code, {
-					lang,
-					theme: 'vesper'
-				})
-			);
-			return `{@html \`${html}\` }`;
-		}
-	},
-	remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
-	rehypePlugins: [rehypeSlug]
+  extensions: ['.md'],
+  layout: {
+    _: './src/mdsvex.svelte'
+  },
+  highlight: {
+    highlighter: async (code, lang = 'text') => {
+      // Reuse the same highlighter instance
+      const highlighter = await highlighterPromise;
+      const html = escapeSvelte(
+        highlighter.codeToHtml(code, {
+          lang,
+          theme: 'vesper'
+        })
+      );
+      return `{@html \`${html}\` }`;
+    }
+  },
+  remarkPlugins: [remarkUnwrapImages, [remarkToc, { tight: true }]],
+  rehypePlugins: [rehypeSlug]
 };
 
 export { mdsvexOptions };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 
-	extensions: ['.svelte', '.md'],
+  extensions: ['.svelte', '.md'],
 
-	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+  kit: {
+    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
+    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
+    adapter: adapter()
+  }
 };
 
 export default config;
